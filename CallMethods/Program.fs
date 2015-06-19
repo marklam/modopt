bfa1aeb7c4bec1805a85446285b665a35b83dc0f
Mark Lambert <marklam@spunow.co.uk>
2015-06-19 20:44:52 +0100

    Updated for VS2015RC, .net 4.5.2, F#4 Uncommented the lines that show the crash

--- CallMethods/Program.fs
+++ CallMethods/Program.fs
@@ -2,10 +2,10 @@
 let main _argv = 
     let cxx = CxxCLI.CxxDerived()
     let cs  = CSharp.CSharpDerived()
-    //let fs  = FSharp.FSharpDerived()
+    let fs  = FSharp.FSharpDerived()
 
     printfn "cxx %d %d" (cxx.GetLong()) (cxx.GetInt()) 
     printfn "cs  %d %d" ( cs.GetLong()) ( cs.GetInt()) 
-    //printfn "fs  %d %d" ( fs.GetLong()) ( fs.GetInt()) 
+    printfn "fs  %d %d" ( fs.GetLong()) ( fs.GetInt()) 
     
     0
